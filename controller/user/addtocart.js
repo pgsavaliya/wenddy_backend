381f5adc176b2ed063c18b27dcb0933a72dee8fa
pgsavaliya <pavansavaliya.itcodehelp@gmail.com>
2022-08-10 11:49:11 +0530

    change all key names

--- controller/user/addtocart.js
+++ controller/user/addtocart.js
@@ -3,10 +3,23 @@ const { response } = require("../../middleware/response");
 
 exports.addtocart = async (req, res) => {
   try {
-    req.body.userId = req.userId;
+    req.body.user_id = req.user_id;
     let resp = await addtocartService.addtocart(req.body);
     if (resp) {
       return response("Added successfully..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
+    }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
+};
+exports.getcart = async (req, res) => {
+  try {
+    req.body.user_id = req.user_id;
+    let resp = await addtocartService.getcart(req.body);
+    if (resp) {
+      return response(resp, resp.data, 200, res);
     } else {
       return response("Something went wrong!!", {}, 500, res);
     }
