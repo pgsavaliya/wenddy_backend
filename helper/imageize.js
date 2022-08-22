be565e33f4d423fb7cd5567b7a5bd4d5198eb3ea
rutu-patel00 <97510829+rutu-patel00@users.noreply.github.com>
2022-08-22 10:00:48 +0530

    ADD: imagesize.js to helper

---
+++ helper/imageize.js
@@ -0,0 +1,20 @@
+// const sharp = require("sharp");
+// const fs = require("fs");
+// const multer = require("multer");
+// const path = require("path")
+// require("dotenv").config({ path: path.join(__dirname, '../config/.env') });
+// const firebaseAdmin = require("firebase-admin");
+// const { v4: uuidv4 } = require('uuid');
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getStorage, deleteObject } = require("firebase-admin/storage");
+// const serviceAccount = require("../helper/wenddys-528d3-firebase-adminsdk-xzrdj-8b705f1d5b.json");
+
+// initializeApp({
+//     credential: cert(serviceAccount),
+// });
+
+// const bucket = getStorage().bucket(
+//     "gs://wenddys-528d3.appspot.com"
+// );
+
+// console.log("process.env.mongoUrl .............",process.env.MONGODB_URL);
