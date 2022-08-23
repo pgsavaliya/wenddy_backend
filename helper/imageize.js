704596bf9f8cdab7d7e9cc4103b4063e35d6a72a
rutu-patel00 <97510829+rutu-patel00@users.noreply.github.com>
2022-08-23 16:16:21 +0530

    ADD: IMAGE TO SHARP

--- helper/imageize.js
+++ helper/imageize.js
@@ -1,187 +1,232 @@
-// const sharp = require("sharp");
-// const fs = require("fs");
-// const multer = require("multer");
-// const path = require("path")
-// require("dotenv").config({ path: path.join(__dirname, '../config/.env') });
-// const firebaseAdmin = require("firebase-admin");
-// const { v4: uuidv4 } = require('uuid');
-// const { initializeApp, cert } = require("firebase-admin/app");
-// const { getStorage, deleteObject } = require("firebase-admin/storage");
-// const serviceAccount = require("./wenddys-528d3-firebase-adminsdk-xzrdj-8b705f1d5b.json");
-// const { rejects } = require("assert");
+const sharp = require("sharp");
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path")
+require("dotenv").config({ path: path.join(__dirname, '../config/.env') });
+const firebaseAdmin = require("firebase-admin");
+const { v4: uuidv4 } = require('uuid');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getStorage, deleteObject } = require("firebase-admin/storage");
+const serviceAccount = require("./wenddys-528d3-firebase-adminsdk-xzrdj-8b705f1d5b.json");
 
-// initializeApp({
-//     credential: cert(serviceAccount),
-// });
+initializeApp({
+    credential: cert(serviceAccount),
+});
 
-// const bucket = getStorage().bucket(
-//     "gs://wenddys-528d3.appspot.com"
-// );
+const bucket = getStorage().bucket(
+    "gs://wenddys-528d3.appspot.com"
+);
 
-// // console.log("path.join(__dirname, '../config/.env') .............", path.join(__dirname, '../config/.env'));
-
+// console.log("path.join(__dirname, '../config/.env') .............", path.join(__dirname, '../config/.env'));
 
 
-// async function uploadFile(image, path, filename, size) {
 
-//     console.log("image from helper ...............", image.path);
-//     console.log("path from helper ...............", path);
-//     console.log("filename from helper ...............", filename);
-//     console.log("size from helper ...............", size);
-//     let media = [];
-//     //firebase logic to upload the image
-//     let uploaded = bucket.upload(path, {
-//         public: true,
-//         destination: `images/${size}/${filename}`,
-//         metadata: {
-//             firebaseStorageDownloadTokens: uuidv4(),
-//         },
-//     });
-//     console.log("73856784582");
-//     console.log("uploaded .................", uploaded);
-//     let data = await uploaded;
-//     // console.log("data ..........",data);
-//     data = data[0]
-//     if (data) {
-//         return media.push({
-//             mediaLink: data.metadata.mediaLink,
-//             name: data.metadata.name
-//         })
-//     }
-//     fs.unlink(image.path, (err) => {
-//         if (err) console.log("someError: ", err)
+async function uploadFile(image, filename, size) {
+
+    try {
+        console.log("image from helper ...............", image);
+        // console.log("path from helper ...............", path);
+        console.log("filename from helper ...............", filename);
+        console.log("size from helper ...............", size);
+        let media = [];
+        //firebase logic to upload the image
+        // let uploaded = bucket.file(path).save(), {
+        //     public: true,
+        //     destination: `images/${size}/${filename}`,
+        //     metadata: {
+        //         firebaseStorageDownloadTokens: uuidv4(),
+        //     },
+        // });
+
+        // let img=Buffer.from
+        let uploaded = await bucket.file(`images/${size}/${filename}`).save(image, {
+            // resumable: false, 
+            metadata: {
+                contentType: 'image/jpg'
+            }
+        })
+        // console.log("73856784582");
+        console.log("uploaded .................", uploaded);
+        // let data = await uploaded;
+        // console.log("data ..........",data);
+        // data = data[0]
+        // if (data) {
+        // return media.push({
+        //     mediaLink: data.metadata.mediaLink,
+        //     name: data.metadata.name
+        // })
+        // }}
+        return uploaded;
+    }
+    catch (err) {
+        console.log("err form uploadFile .....", err);
+    }
+    // fs.unlink(image.path, (err) => {
+    //     if (err) console.log("someError: ", err)
+    // })
+}
+
+
+async function uploadReduceFile(file) {
+    console.log("fillllllllllllllllllllllllllllllll", file)
+
+
+    // return await  sharp(file, {raw})
+    //   .toBuffer()
+    //   .then(data => {
+    //     // 100 pixels high, auto-scaled width
+    //     console.log("output; ", data)
+    //   }).catch(err => {
+    //             console.log("err reduce file... .......",err);
+    //             return err
+    //         })
+
+
+    return await sharp(file)
+        .resize(520, 240)
+        .toBuffer()
+        // .toFile("test.jpg")
+        .then((outputBuffer) => {
+            console.log("outputBuffer ......", outputBuffer);
+            return outputBuffer
+        }).catch(err => {
+            console.log("err reduce file... .......", err);
+            return err
+        })
+}
+
+// image modification 
+// async function uploadReduceFile(file) {
+//     console.log("file from reduce file ............", file);
+//     return 
+//     // let reduceImage= await sharp({
+//     //     create:{
+//     //         width:20,
+//     //         height:20,
+//     //         channels:4,
+//     //         background:{r: 255, g: 0, b: 0, alpha: 0.5},
+//     //     }
+//     // }).png().jpeg();
+//     // console.log("reduceImage ...........",reduceImage);
+//     // return reduceImage;
+//     .toBuffer()
+//     // .toFile(file)
+//     .then((file) => {
+//         console.log("buffer from reduce ........", file);
+//         return file;
+//     }).catch(err => {
+//         console.log("this is rror: ", err)
 //     })
 // }
 
-// // image modification 
-// async function uploadReduceFile(file) {
-//     console.log("file from reduce file ............", file);
-//     let reduceImage= await sharp({
-//         create:{
-//             width:20,
-//             height:20,
-//             channels:4,
-//             background:{r: 255, g: 0, b: 0, alpha: 0.5},
-//         }
-//     }).png().jpeg();
-//     console.log("reduceImage ...........",reduceImage);
-//     return reduceImage;
-//     // .toBuffer()
-//     // .toFile(file)
-//     // .then((file) => {
-//     //     console.log("buffer from reduce ........", file);
-//     //     return file;
-//     // }).catch(err => {
-//     //     console.log("this is rror: ", err)
-//     // })
-// }
+async function mediumImg(img) {
+    // console.log("file from medium file ............", img);
+    let mediumImage = await sharp({
+        create: {
+            width: 100,
+            height: 100,
+            channels: 4,
+            background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+        }
+    }).png().jpeg();
+    console.log("mediumImage ...........", mediumImage);
+    return mediumImage;
+    // .toFile(`../upload/medium/${img.filename}`, (err, info) => {
+    //     if (err) {
+    //         return err
+    //     }
+    //     else {
+    //         console.log("buffer from small ........", info);
+    //         return info
+    //     }
+    // });
+    // .then(async img => {
+    //     console.log("buffer from medium ........", img);
+    //     return img
+    // })
+    // .catch(err => {
+    //     return err
+    // })
+}
 
-// async function mediumImg(img) {
-//     // console.log("file from medium file ............", img);
-//     let mediumImage= await sharp({
-//         create:{
-//             width:100,
-//             height:100,
-//             channels:4,
-//             background:{r: 255, g: 0, b: 0, alpha: 0.5},
-//         }
-//     }).png().jpeg();
-//     console.log("mediumImage ...........",mediumImage);
-//     return mediumImage;
-//     // .toFile(`../upload/medium/${img.filename}`, (err, info) => {
-//     //     if (err) {
-//     //         return err
-//     //     }
-//     //     else {
-//     //         console.log("buffer from small ........", info);
-//     //         return info
-//     //     }
-//     // });
-//     // .then(async img => {
-//     //     console.log("buffer from medium ........", img);
-//     //     return img
-//     // })
-//     // .catch(err => {
-//     //     return err
-//     // })
-// }
+async function smallImg(file) {
+    console.log("file from small file ............", file);
+    let smallImage = await sharp({
+        create: {
+            width: 50,
+            height: 20,
+            channels: 4,
+            background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+        }
+    }).png().jpeg();
+    console.log("smallImage ...........", smallImage);
+    return smallImage;
+    // .then((err, file) => {
+    //     if (err) {
+    //         return err
+    //     }
+    //     else {
+    //         console.log("buffer from small ........", file);
+    //         return file
+    //     }
+    // })
+}
 
-// async function smallImg(file) {
-//     console.log("file from small file ............", file);
-//     let smallImage= await sharp({
-//         create:{
-//             width:50,
-//             height:20,
-//             channels:4,
-//             background:{r: 255, g: 0, b: 0, alpha: 0.5},
-//         }
-//     }).png().jpeg();
-//     console.log("smallImage ...........",smallImage);
-//     return smallImage;
-//     // .then((err, file) => {
-//     //     if (err) {
-//     //         return err
-//     //     }
-//     //     else {
-//     //         console.log("buffer from small ........", file);
-//     //         return file
-//     //     }
-//     // })
-// }
+async function uploadImage(image, filename, size) {
+    let imageData = {};
+    for (const [index, siz] of size.entries()) {
+        console.log("size ........", siz);
+        switch (siz) {
+            // for original size
+            case 1: {
+                // let imageBuffer = fs.createReadStream(image.path)
+                let awsImg = await uploadFile(image, filename, "originalSize")
+                console.log("awsImg ...........", awsImg);
+                imageData["original"] = awsImg;
+                console.log("imageData ...........", imageData);
+                // imageData["small"] = awsImg.Key
+                // imageData["originalURL"] = awsImg.Location
+                break
+            }
+            //for reduce size
+            case 2: {
+                // console.log("here iamge;", image)
+                // console.log(".....reduct file: ", await uploadReduceFile(image))
+                let awsImg = await uploadFile(await uploadReduceFile(image), filename, "reduceSize");
+                console.log("awsImg ...........", awsImg);
+                imageData["reduce"] = awsImg;
+                console.log("imageData ...........", imageData);
+                // imageData["small"] = awsImg.Key
+                // imageData["reduceURL"] = awsImg.Location
+                break
+            }
+            case 3: {
+                // console.log("here iamge ............", image)
+                // console.log(".....reduct file: ", await mediumImg(image))
+                let awsImg = (await uploadFile(await mediumImg(image), image.path, image.filename, "mediumSize"))
+                imageData["medium"] = awsImg;
+                console.log("imageData ...........", imageData);
+                // imageData["medium"] = awsImg.Key
+                // imageData["mediumURL"] = awsImg.Location
 
-// async function uploadImage(image, size) {
-//     let imageData = {};
-//     for (const [index, siz] of size.entries()) {
-//         console.log("size ........", siz);
-//         switch (siz) {
-//             // for original size
-//             case 1: {
-//                 let imageBuffer = fs.createReadStream(image.path)
-//                 let awsImg = (await uploadFile(image, image.path, image.filename, "originalSize"))
-//                 imageData["original"] = awsImg;
-//                 console.log("imageData ...........", imageData);
-//                 // imageData["small"] = awsImg.Key
-//                 // imageData["originalURL"] = awsImg.Location
-//                 break
-//             }
-//             //for reduce size
-//             case 2: {
-//                 // console.log("here iamge;", image)
-//                 // console.log(".....reduct file: ", await uploadReduceFile(image))
-//                 let awsImg = (await uploadFile(await uploadReduceFile(image), image.path, image.filename, "reduceSize"))
-//                 imageData["reduce"] = awsImg;
-//                 console.log("imageData ...........", imageData);
-//                 // imageData["small"] = awsImg.Key
-//                 // imageData["reduceURL"] = awsImg.Location
-//                 break
-//             }
-//             case 3: {
-//                 // console.log("here iamge ............", image)
-//                 // console.log(".....reduct file: ", await mediumImg(image))
-//                 let awsImg = (await uploadFile(await mediumImg(image), image.path, image.filename, "mediumSize"))
-//                 imageData["medium"] = awsImg;
-//                 console.log("imageData ...........", imageData);
-//                 // imageData["medium"] = awsImg.Key
-//                 // imageData["mediumURL"] = awsImg.Location
+                break
+            }
+            case 4: {
+                let awsImg = (await uploadFile(await smallImg(image), image.path, image.filename, "smallSize"))
+                imageData["small"] = awsImg;
+                console.log("imageData ...........", imageData);
+                // imageData["small"] = awsImg.Key
+                // imageData["smallURL"] = awsImg.Location
+                break
+            }
 
-//                 break
-//             }
-//             case 4: {
-//                 let awsImg = (await uploadFile(await smallImg(image), image.path, image.filename, "smallSize"))
-//                 imageData["small"] = awsImg;
-//                 console.log("imageData ...........", imageData);
-//                 // imageData["small"] = awsImg.Key
-//                 // imageData["smallURL"] = awsImg.Location
-//                 break
-//             }
+            default: return { message: "plz enter valid size type", success: false, undefined, status: 400 }
+        }
+        if (index === size.length - 1) {
+            fs.unlinkSync(image.path)
+            return imageData
+        }
+    }
+}
 
-//             default: return { message: "plz enter valid size type", success: false, undefined, status: 400 }
-//         }
-//         if (index === size.length - 1) {
-//             fs.unlinkSync(image.path)
-//             return imageData
-//         }
-//     }
-// }
-
-// module.exports = { uploadImage }
+module.exports = { uploadImage }
--- service/image.js
+++ service/image.js
@@ -1,70 +1,74 @@
-const firebaseAdmin = require("firebase-admin");
-const { v4: uuidv4 } = require('uuid');
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getStorage, deleteObject } = require("firebase-admin/storage");
-const serviceAccount = require("../helper/wenddys-528d3-firebase-adminsdk-xzrdj-8b705f1d5b.json");
-const fs = require("fs");
-const e = require("express");
-// const path = require("path");
+// const firebaseAdmin = require("firebase-admin");
+// const { v4: uuidv4 } = require('uuid');
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getStorage, deleteObject } = require("firebase-admin/storage");
+// const serviceAccount = require("../helper/wenddys-528d3-firebase-adminsdk-xzrdj-8b705f1d5b.json");
+// const fs = require("fs");
+// const e = require("express");
+// // const path = require("path");
 
-initializeApp({
-    credential: cert(serviceAccount),
-});
+// initializeApp({
+//     credential: cert(serviceAccount),
+// });
 
-const bucket = getStorage().bucket(
-    "gs://wenddys-528d3.appspot.com"
-);
+// const bucket = getStorage().bucket(
+//     "gs://wenddys-528d3.appspot.com"
+// );
 
 
-// const { uploadImage } = require("../helper/imageize");
+const { uploadImage } = require("../helper/imageize");
 
 module.exports = {
     upload: async (image) => {
         return new Promise(async (res, rej) => {
             try {
-                //firebase logic to upload the image
-                let i;
-                let media = [];
-                // console.log("image .....", image);
-                let uploaded = bucket.upload(image.path, {
-                    public: true,
-                    destination: `images/${Math.random() * 10000 + image.filename}`,
-                    metadata: {
-                        firebaseStorageDownloadTokens: uuidv4(),
-                    },
-                });
-                let data = await uploaded;
-                data = data[0]
-                if (data) {
-                    // fs.unlinkSync(image.path);
-                    media.push({
-                        mediaLink: data.metadata.mediaLink,
-                        name: data.metadata.name
-                    })
-                    res({
-                        status: 200,
-                        data: media
-                    })
-                    console.log('image ..........', image)
-                    fs.unlink(image.path, (err) => {
-                        if (err) console.log("someError: ", err)
-                    })
-                }
-                else {
-                    rej({ status: 404, message: "something went wrong!!" });
-                }
-                // console.log("image .......", image);
-                // let result = await uploadImage(image, [1,2, 3,4]);
-                // console.log("result .............", result);
-                // if (result) {
+                // //firebase logic to upload the image
+                // let i;
+                // let media = [];
+                // // console.log("image .....", image);
+                // let uploaded = bucket.upload(image.path, {
+                //     public: true,
+                //     destination: `images/${Math.random() * 10000 + image.filename}`,
+                //     metadata: {
+                //         firebaseStorageDownloadTokens: uuidv4(),
+                //     },
+                // });
+                // let data = await uploaded;
+                // data = data[0]
+                // if (data) {
+                //     // fs.unlinkSync(image.path);
+                //     media.push({
+                //         mediaLink: data.metadata.mediaLink,
+                //         name: data.metadata.name
+                //     })
                 //     res({
                 //         status: 200,
-                //         data: result
-                //     });
+                //         data: media
+                //     })
+                //     console.log('image ..........', image)
+                //     fs.unlink(image.path, (err) => {
+                //         if (err) console.log("someError: ", err)
+                //     })
                 // }
                 // else {
                 //     rej({ status: 404, message: "something went wrong!!" });
                 // }
+                console.log("image from service .......", image);
+                // let imageBuffer = Buffer.from(image);
+                let imageStr = image.toString('base64')
+                let imageBuffer = Buffer.from(imageStr, 'base64');
+                console.log("imageBuffer ...........", imageBuffer);
+                let result = await uploadImage(imageBuffer, image.filename,[1, 2]);
+                console.log("result .............", result);
+                if (result) {
+                    res({
+                        status: 200,
+                        data: result
+                    });
+                }
+                else {
+                    rej({ status: 404, message: "something went wrong!!" });
+                }
             }
             catch (err) {
                 console.log("error ...", err);
