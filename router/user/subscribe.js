14539962c88d664d65b293544f3e0d01c87bc2a6
rutu-patel00 <97510829+rutu-patel00@users.noreply.github.com>
2022-08-30 14:00:09 +0530

    FIX: user=>product conflict

--- router/user/subscribe.js
+++ router/user/subscribe.js
@@ -7,10 +7,6 @@ subscribeRoute.get("/", (req, res) => {
   res.send({ status: 200, message: "subscribe route is working" });
 });
 
-<<<<<<< HEAD
 subscribeRoute.post("/add",subscribecontroller.addsubscribe);
-=======
-subscribeRoute.post("/addsubscribe", subscribecontroller.addsubscribe);
->>>>>>> b52afbdb440de78d8673a9ab4062d6da7a9e1672
 
 module.exports = subscribeRoute;
