89c41c0b509c14007c0f0add3a066648cc897729
rutu-patel00 <97510829+rutu-patel00@users.noreply.github.com>
2022-08-31 11:45:48 +0530

    FIX: response

--- service/admin/country.js
+++ service/admin/country.js
@@ -142,7 +142,7 @@ module.exports = {
       try {
         let getData = await countryModel.find();
         if (getData) {
-          res({ status: 200, data: { data: getData } });
+          res({ status: 200, data: getData});
         } else {
           rej({ status: 404, message: "Data Not Found!!" });
         }
