80b5490db3fca5a93ccef2e046e55dde5999a3d3
pgsavaliya <pavansavaliya.itcodehelp@gmail.com>
2022-09-07 14:24:43 +0530

    WIP

--- controller/user/address.js
+++ controller/user/address.js
@@ -2,44 +2,67 @@ const orderService = require("../../service/user/address");
 const { response } = require("../../middleware/response");
 
 exports.add = async (req, res) => {
-    try {
-        let resp = await orderService.add(req.user_id, req.body);
-        if (resp) {
-            return response("Added successfully..!!", resp.data, 200, res);
-        } else {
-            return response("Something went wrong!!", {}, 500, res);
-        }
-    } catch (err) {
-        return response(err.message, err?.error, err.status, res);
+  try {
+    let resp = await orderService.add(req.user_id, req.body);
+    if (resp) {
+      return response("Added successfully..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
     }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
 };
 
 exports.addMultiAddress = async (req, res) => {
-    try {
-        let resp = await orderService.addMultiAddress(req.user_id, req.params._id, req.body);
-        if (resp) {
-            return response("SUCCESS..!!", resp.data, 200, res);
-        } else {
-            return response("Something went wrong!!", {}, 500, res);
-        }
-    } catch (err) {
-        return response(err.message, err?.error, err.status, res);
+  try {
+    let resp = await orderService.addMultiAddress(
+      req.user_id,
+      req.params._id,
+      req.body
+    );
+    if (resp) {
+      return response("SUCCESS..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
     }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
 };
 
 exports.update = async (req, res) => {
-    try {
-        let resp = await orderService.update(req.user_id, req.params.address_id, req.body.address);
-        if (resp) {
-            return response("SUCCESS..!!", resp.data, 200, res);
-        } else {
-            return response("Something went wrong!!", {}, 500, res);
-        }
-    } catch (err) {
-        return response(err.message, err?.error, err.status, res);
+  try {
+    let resp = await orderService.update(
+      req.user_id,
+      req.params.address_id,
+      req.body.address
+    );
+    if (resp) {
+      return response("SUCCESS..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
     }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
 };
-
+exports.deleteone = async (req, res) => {
+  try {
+    let resp = await orderService.deleteone(
+      req.user_id,
+      req.params.address_id,
+      req.body.address
+    );
+    if (resp) {
+      return response("SUCCESS..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
+    }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
+};
 // exports.primaryUpdate = async (req, res) => {
 //     try {
 //         let resp = await orderService.primaryUpdate(req.user_id, req.params._id, req.body);
@@ -54,27 +77,27 @@ exports.update = async (req, res) => {
 // };
 
 exports.getAll = async (req, res) => {
-    try {
-        let resp = await orderService.getAll(req.user_id);
-        if (resp) {
-            return response("SUCCESS..!!", resp.data, 200, res);
-        } else {
-            return response("Something went wrong!!", {}, 500, res);
-        }
-    } catch (err) {
-        return response(err.message, err?.error, err.status, res);
+  try {
+    let resp = await orderService.getAll(req.user_id);
+    if (resp) {
+      return response("SUCCESS..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
     }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
 };
 
 exports.delete = async (req, res) => {
-    try {
-        let resp = await orderService.delete(req.params._id);
-        if (resp) {
-            return response("Deleted successfully..!!", resp.data, 200, res);
-        } else {
-            return response("Something went wrong!!", {}, 500, res);
-        }
-    } catch (err) {
-        return response(err.message, err?.error, err.status, res);
+  try {
+    let resp = await orderService.delete(req.params._id);
+    if (resp) {
+      return response("Deleted successfully..!!", resp.data, 200, res);
+    } else {
+      return response("Something went wrong!!", {}, 500, res);
     }
+  } catch (err) {
+    return response(err.message, err?.error, err.status, res);
+  }
 };
