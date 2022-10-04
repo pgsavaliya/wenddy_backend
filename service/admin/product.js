2b88f655a8a21016be0900993bb0a4a6e3d8ff43
pgsavaliya <pavan8866116973@gmail.com>
2022-10-04 10:38:33 +0530

    add pagination error in require file

--- service/admin/product.js
+++ service/admin/product.js
@@ -112,96 +112,100 @@ module.exports = {
   },
 
   getAll: (page, limit, str, startDate, endDate, status) => {
-    return new Promise(async (res, rej) => {
-      try {
-        let qry = {};
-        page = parseInt(page);
-        limit = parseInt(limit);
-        if (startDate && endDate) {
-          startDate = new Date(startDate);
-          endDate = new Date(endDate);
-          endDate.setDate(endDate.getDate() + 1);
-          qry["$and"] = [
-            { createdAt: { $gt: startDate } },
-            { createdAt: { $lt: endDate } },
-          ];
-        }
-        // console.log(str);
-        if (str) {
-          qry["$or"] = [{ product_title: { $regex: str, $options: "i" } }];
-        }
-        // if (status) {
-        //     qry["status"] = status;
-        // }
-        let getData = await productModel.aggregate([
-          { $match: qry },
-          //   {
-          //     $lookup: {
-          //       from: "transactions",
-          //       localField: "_id",
-          //       foreignField: "idOfCompanyProperty",
-          //       as: "propertyTransactionData",
-          //     },
-          //   },
-          // { $unwind: "$propertyTransactionData" },
-          {
-            $facet: {
-              total_count: [
-                {
-                  $group: {
-                    _id: null,
-                    count: { $sum: 1 },
+    if (page && limit) {
+      return new Promise(async (res, rej) => {
+        try {
+          let qry = {};
+          page = parseInt(page);
+          limit = parseInt(limit);
+          if (startDate && endDate) {
+            startDate = new Date(startDate);
+            endDate = new Date(endDate);
+            endDate.setDate(endDate.getDate() + 1);
+            qry["$and"] = [
+              { createdAt: { $gt: startDate } },
+              { createdAt: { $lt: endDate } },
+            ];
+          }
+          // console.log(str);
+          if (str) {
+            qry["$or"] = [{ product_title: { $regex: str, $options: "i" } }];
+          }
+          // if (status) {
+          //     qry["status"] = status;
+          // }
+          let getData = await productModel.aggregate([
+            { $match: qry },
+            //   {
+            //     $lookup: {
+            //       from: "transactions",
+            //       localField: "_id",
+            //       foreignField: "idOfCompanyProperty",
+            //       as: "propertyTransactionData",
+            //     },
+            //   },
+            // { $unwind: "$propertyTransactionData" },
+            {
+              $facet: {
+                total_count: [
+                  {
+                    $group: {
+                      _id: null,
+                      count: { $sum: 1 },
+                    },
                   },
-                },
-              ],
-              result: [
-                {
-                  $project: {
-                    __v: 0,
+                ],
+                result: [
+                  {
+                    $project: {
+                      __v: 0,
+                    },
                   },
-                },
-                { $sort: { createdAt: -1 } },
-                { $skip: (page - 1) * limit },
-                { $limit: limit },
-              ],
+                  { $sort: { createdAt: -1 } },
+                  { $skip: (page - 1) * limit },
+                  { $limit: limit },
+                ],
+              },
             },
-          },
-        ]);
-        getData = getData[0];
-        // console.log("getData.total_count[0] ...........", getData.total_count);
-        // console.log("getData ...........", getData);
-        if (getData.result.length > 0) {
-          //   getData.result = getData.result.map((item, index) => {
-          //     let uniqueUser = [
-          //       ...new Set(
-          //         item.toatlInvestedData.map((i) => i.customerId.toString())
-          //       ),
-          //     ].length;
+          ]);
+          getData = getData[0];
+          // console.log("getData.total_count[0] ...........", getData.total_count);
+          // console.log("getData ...........", getData);
+          if (getData.result.length > 0) {
+            //   getData.result = getData.result.map((item, index) => {
+            //     let uniqueUser = [
+            //       ...new Set(
+            //         item.toatlInvestedData.map((i) => i.customerId.toString())
+            //       ),
+            //     ].length;
 
-          //     delete item.toatlInvestedData;
-          //     return {
-          //       ...item,
-          //       uniqueUser,
-          //       fundedByUser:
-          //         (item.totalInvestedAmountByUser / item.totalAmount) * 100 || 0,
-          //       index: getData.total_count[0].count - index,
-          //     };
-          //   });
-          res({
-            status: 200,
-            data: {
-              total_count: getData.total_count[0].count,
-              result: getData.result,
-            },
-          });
-        } else {
-          rej({ status: 404, message: "No Data Found!!" });
+            //     delete item.toatlInvestedData;
+            //     return {
+            //       ...item,
+            //       uniqueUser,
+            //       fundedByUser:
+            //         (item.totalInvestedAmountByUser / item.totalAmount) * 100 || 0,
+            //       index: getData.total_count[0].count - index,
+            //     };
+            //   });
+            res({
+              status: 200,
+              data: {
+                total_count: getData.total_count[0].count,
+                result: getData.result,
+              },
+            });
+          } else {
+            rej({ status: 404, message: "No Data Found!!" });
+          }
+        } catch (err) {
+          console.log("err ....", err);
+          rej({ status: 500, error: err, message: "something went wrong!!" });
         }
-      } catch (err) {
-        console.log("err ....", err);
-        rej({ status: 500, error: err, message: "something went wrong!!" });
-      }
-    });
+      });
+    } else {
+      return "pagination is require....";
+    }
   },
 
   byId: (id) => {
