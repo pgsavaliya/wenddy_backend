a2e0bfd6bece9688d1c12515d4772506a75e8fca
rutu-patel00 <97510829+rutu-patel00@users.noreply.github.com>
2022-10-05 10:38:05 +0530

    CHANGE: uncomment code in user=>order

--- service/user/order.js
+++ service/user/order.js
@@ -8,24 +8,26 @@ module.exports = {
     return new Promise(async (res, rej) => {
       try {
         // let newData1 = [];
+        console.log("4vnruiegnwurhnuwruwehguweip");
         if (data.payment_data.payments.captures[0].status == 'COMPLETED') {
-          // let newData = await data.product.map(async (item) => {
-          //   let productData = await productModel.findOne({ uniqueCode: item.product_id });
-          //   // console.log("item .....", productData);
-          //   item["product_title"] = productData.product_title;
-          //   item["product_image"] = productData.image[0];
-          //   let findData = await cartModel.findById(item.cart_id);
-          //   // console.log("findData .......", findData);
-          //   if (findData) {
-          //     // console.log("4t7892y4thu");
-          //     // let cartData = await cartModel.deleteMany({ 'product.cart_id': item.cart_id, user_id: data.user_id });
-          //   }
-          //   console.log("item .......", item);
-          //   newData1.concat(item);
-          // })
+          console.log("435y3784y3hnfui");
+          let newData = await data.product.map(async (item) => {
+            // let productData = await productModel.findOne({ uniqueCode: item.product_id });
+            //   // console.log("item .....", productData);
+            //   item["product_title"] = productData.product_title;
+            //   item["product_image"] = productData.image[0];
+            let findData = await cartModel.findById(item.cart_id);
+            //   // console.log("findData .......", findData);
+            if (findData) {
+              // console.log("4t7892y4thu");
+              let cartData = await cartModel.deleteMany({ _id: mongoose.Types.ObjectId(item.cart_id), user_id: mongoose.Types.ObjectId(data.user_id) });
+                console.log("cartData .......", cartData);
+            }
+            //   newData1.concat(item);
+          })
           // console.log("newData1 .......", newData1);
           // console.log("xyz .......", xyz);
-          console.log("data ........", data);
+          // console.log("data ........", data);
           let neworderModel = new orderModel(data);
           let saveData = await neworderModel.save();
           // res(saveData);
